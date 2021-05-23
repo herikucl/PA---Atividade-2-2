931970b5c231640f4e4202b61e3b2bbb72e7de92
Hérik Lemos <>
2021-05-23 15:53:05 +0000

    Finalizando função

--- main.cpp
+++ main.cpp
@@ -2,16 +2,34 @@
 using namespace std;
 
 void maxmin(int *p,int n, int &ma, int &mi){
+  int maior=0,menor=999999,*pontaux;
 
+    for(int i=0;i<n;i++){
+      if(p[i]>maior){
+        maior=p[i];
+      }
+      if(p[i]<menor){
+        menor=p[i];
+      }
+    }
+  pontaux=&ma;
+  *pontaux=maior;
+  pontaux=&mi;
+  *pontaux=menor;
 }
 
 
 int main() {
- int n,max,min,*pont;
- cin>>n;
- int vet[n];
- for(int i=0;i<n;i++){
+ int N,max,min,*pont;
+ cin>>N;
+ int vet[N];
+ for(int i=0;i<N;i++){
    cin>>vet[i];
  }
+pont=vet;
+maxmin(pont,N,max,min);
+
+
+
 
 }

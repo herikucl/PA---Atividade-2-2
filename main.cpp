2111c929f61f6a303b830ae9e09644fc33667c71
Hérik Lemos <>
2021-05-23 15:48:28 +0000

    Criação de parâmetros da função e preenchimento do vetor

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 using namespace std;
 
+void maxmin(int *p,int n, int &ma, int &mi){
+
+}
 
 
 int main() {
+ int n,max,min,*pont;
+ cin>>n;
+ int vet[n];
+ for(int i=0;i<n;i++){
+   cin>>vet[i];
+ }
 
 }
